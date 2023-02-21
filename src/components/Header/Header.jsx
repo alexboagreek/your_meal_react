3e3c16bee5f754046c96253356e29eacc9971107
Alex <olivesargreek@gmail.com>
2023-02-21 14:46:06 +0300

    feat: добавлены компоненты Navigation, Catalog & Order

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,20 +4,19 @@ import { Container } from '../Container/Container';
 
 export const Header = () => (
     <header className={styles.header}>
-      <Container class={styles.header__container}>
-        <img class={styles.header__logo} src={logo} alt="Логотип YourMeal" />
+      <Container>
+        <div className={styles.container}>
+          <img class={styles.logo} src={logo} alt="Логотип YourMeal" />
 
-        <div class={styles.header__wrapper}>
-          <h1 class={styles.header__title}>
-            <span>Только самые</span>
-            <span class={styles.header__red}>сочные бургеры!</span>
-          </h1>
+          <div class={styles.wrapper}>
+            <h1 class={styles.title}>
+              <span>Только самые</span>
+              <span class={styles.red}>сочные бургеры!</span>
+            </h1>
 
-          <p class={styles.header__appeal}>Бесплатная доставка от 599₽</p>
+            <p class={styles.appeal}>Бесплатная доставка от 599₽</p>
+          </div>
         </div>
       </Container>
     </header>
-       
-    
-  
 );
